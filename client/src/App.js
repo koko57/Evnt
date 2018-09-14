d0caaaa234d6966ae91e60b8cff6f9f4e195687b
Aggie Koko <agata.kosior57@gmail.com>
2018-09-14 20:30:39 +0200

    create client-side with create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import AddEvent from './components/AddEvent';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
