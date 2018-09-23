854fdc9b3f43f9023138781843fa51e649f7aa9e
Aggie Koko <agata.kosior57@gmail.com>
2018-09-23 20:13:35 +0200

    Move backend files to separate server dir.

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  google: {
+    clientID: '879097420045-vep3k7l19uf4uj85e4falrafuo1odbqq.apps.googleusercontent.com',
+    clientSecret: '67p60TVuMH_bZuPsYTD0wEGh'
+  },
+  cookie: 'jhbcjhsbajhcbsjabcjbsaj'
+};
