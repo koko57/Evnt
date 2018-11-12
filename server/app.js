47377b562e03351f36d8aec087ba4142bba4df58
Aggie Koko <agata.kosior57@gmail.com>
2018-11-12 11:37:14 +0100

    Add jwt strategy

--- server/app.js
+++ server/app.js
@@ -7,6 +7,9 @@ const LocalStrategy = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
 const events = require('./routes/events');
 const auth = require('./routes/auth');
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 const app = express();
 
@@ -32,14 +35,39 @@ app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new LocalStrategy(User.authenticate()));
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    User.authenticate()
+  )
+);
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'ncnjznckz'
+    },
+    function(jwtPayload, cb) {
+      return User.findById(jwtPayload.id)
+        .then(user => {
+          return cb(null, user);
+        })
+        .catch(err => {
+          return cb(err);
+        });
+    }
+  )
+);
 
 app.use('/api/events', events);
 app.use('/api/auth', auth);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5004;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,76 +1,66 @@
-import User from '../models/user';
-import bodyParser from 'body-parser';
-import passport from 'passport';
+const User = require('../models/user');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const AuthController = {};
-import jwt from 'jsonwebtoken';
-import { generateToken, respond } from '../middleware/authMiddleware';
 
 AuthController.register = async (req, res) => {
-    try{
-        User.register(new User({ username: req.body.email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            }), req.body.password, function(err, account) {
-            if (err) {
-                return res.status(500).send('An error occurred: ' + err);
-            }
-
-            passport.authenticate(
-                'local', {
-                    session: false
-                })(req, res, () => {
-                res.status(200).send('Successfully created new account');
-            });
+  const { email, password } = req.body;
+  try {
+    User.register(
+      new User({
+        username: email
+      }),
+      password,
+      err => {
+        if (err) {
+          return res.status(500).send('Error: ' + err);
+        }
+        passport.authenticate('local', {
+          session: false
+        })(req, res, () => {
+          res.status(200).send('New account created');
         });
-    }
-    catch(err){
-        return res.status(500).send('An error occurred: ' + err);
-    }
+      }
+    );
+  } catch (err) {
+    return res.status(500).send('Error: ' + err);
+  }
 };
 
 AuthController.login = async (req, res, next) => {
-    try {
-        if (!req.body.email || !req.body.password) {
-            return res.status(400).json({
-                message: 'Something is not right with your input'
-            });
+  try {
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).json({
+        message: 'Something is not right with your input'
+      });
+    }
+    passport.authenticate('local', { session: false }, (err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          message: 'Invalid username'
+        });
+      }
+      req.login(user, { session: false }, err => {
+        if (err) {
+          res.send(err);
         }
-        passport.authenticate('local', {session: false}, (err, user, info) => {
-            if (err || !user) {
-                return res.status(400).json({
-                    message: 'Something is not right',
-                    user   : user
-                });
-            }
-            req.login(user, {session: false}, (err) => {
-                if (err) {
-                    res.send(err);
-                }
-                // generate a signed son web token with the contents of user object and return it in the response
-                const token = jwt.sign({ id: user.id, email: user.username}, 'ILovePokemon');
-                return res.json({user: user.username, token});
-            });
-        })(req, res);
-    }
-    catch(err){
-        console.log(err);
-    }
+        const token = jwt.sign(
+          { id: user.id, email: user.username },
+          'secretkey'
+        );
+        return res.send({ user, token });
+      });
+    })(req, res);
+  } catch (err) {
+    console.log(err);
+  }
 };
-
-
 
 AuthController.logout = async (req, res) => {
-    req.logout();
-    res.status(200).send('Successfully logged out');
+  req.logout();
+  console.log('loggedOut');
+  res.status(200).send('Successfully logged out');
 };
 
-AuthController.profile = async (req, res) => {
-    try{
-        res.send(req.user);
-    }
-    catch(err){
-        res.send("No user Returned");
-    }
-};
-
-export default AuthController;
+module.exports = AuthController;
